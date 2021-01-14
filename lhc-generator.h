ae2b6b58f0e22cea9a1de53d0ec87d2994d9ee90
Olaf Mersmann <olafm@p-value.net>
2021-01-14 02:15:56 +0100

    Format source code.

--- lhc-generator.h
+++ lhc-generator.h
@@ -1,7 +1,7 @@
-//lese datei nur einmal pro kompilieren ein
+// lese datei nur einmal pro kompilieren ein
 #pragma once
 
-#include <vector>  //https://en.cppreference.com/w/cpp/container/vector
+#include <vector> //https://en.cppreference.com/w/cpp/container/vector
 
 typedef std::vector<double> vector_t;
 
--- main.cc
+++ main.cc
@@ -1,39 +1,43 @@
-//TEST() arguments go from general to specific. The first argument is the name of the test suite, and the second argument is the test's name within the test suite.
+// TEST() arguments go from general to specific. The first argument is the name
+// of the test suite, and the second argument is the test's name within the test
+// suite.
 
-//https://en.cppreference.com/w/cpp/container/vector
-//benutzt wurde dann std::vector<int> v; aber wäre das notwendig gewesen?
-//bzw ist alles der std library automatisch verfügbar und wenn ja warum musste man
-//hier vector extra includen?
+// https://en.cppreference.com/w/cpp/container/vector
+// benutzt wurde dann std::vector<int> v; aber wäre das notwendig gewesen?
+// bzw ist alles der std library automatisch verfügbar und wenn ja warum musste
+// man hier vector extra includen?
 
 #include <gtest/gtest.h> //https://github.com/google/googletest/blob/master/googletest/docs/primer.md
 
 #include "absl/strings/numbers.h" //https://abseil.io/docs/cpp/guides/format
 #include "fmt/core.h" //https://github.com/fmtlib/fmt (in and out streaming)
 
-
-
 #include "lhc-generator.h"
 
 int main(int argc, char *argv[]) {
 
-  size_t number_of_points, number_of_dimensions; //definiert zwei variablen des typs "size_t" https://en.cppreference.com/w/c/types/size_t
-  if (argc != 3) { //wenn nicht genau 3 argumente gegeben werden dann:
+  size_t number_of_points,
+      number_of_dimensions; // definiert zwei variablen des typs "size_t"
+                            // https://en.cppreference.com/w/c/types/size_t
+  if (argc != 3) { // wenn nicht genau 3 argumente gegeben werden dann:
     fmt::print(
         "Usage: {} <n> <d>\n n - Number of points\n d - Number of dimensions\n",
         argv[0]);
     return 1;
   }
 
-// Converts the given string (optionally followed or preceded by ASCII
-// whitespace) into an integer value, returning `true` if successful. The string
-// must reflect a base-10 integer whose value falls within the range of the
-// integer type (optionally preceded by a `+` or `-`). If any errors are
-// encountered, this function returns `false`, leaving `out` in an unspecified
-// state.
-//template <typename int_type>
-//ABSL_MUST_USE_RESULT bool SimpleAtoi(absl::string_view str, int_type* out);
+  // Converts the given string (optionally followed or preceded by ASCII
+  // whitespace) into an integer value, returning `true` if successful. The
+  // string must reflect a base-10 integer whose value falls within the range of
+  // the integer type (optionally preceded by a `+` or `-`). If any errors are
+  // encountered, this function returns `false`, leaving `out` in an unspecified
+  // state.
+  // template <typename int_type>
+  // ABSL_MUST_USE_RESULT bool SimpleAtoi(absl::string_view str, int_type* out);
 
-  if (!absl::SimpleAtoi(argv[1], &number_of_points)) { //Zweites Argument der Funktion ist der Speicherort
+  if (!absl::SimpleAtoi(argv[1],
+                        &number_of_points)) { // Zweites Argument der Funktion
+                                              // ist der Speicherort
     fmt::print("Du Du Du! Keine gültige Zahl als erstes Argument.\n");
     return 1;
   }
@@ -48,7 +52,8 @@ int main(int argc, char *argv[]) {
     if (d > 0) {
       Shuffle(&x);
     }
-    X.push_back(x); //https://www.cplusplus.com/reference/vector/vector/push_back/
+    X.push_back(
+        x); // https://www.cplusplus.com/reference/vector/vector/push_back/
   }
 
   for (size_t n = 0; n < number_of_points; ++n) {
@@ -59,5 +64,4 @@ int main(int argc, char *argv[]) {
   }
 
   return 0;
-
 }
